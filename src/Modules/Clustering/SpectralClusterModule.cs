bbfada0efe616f8e6db0bfd6bfdb098efe3f54bf
gbull122 <gbull122@gmail.com>
2021-03-08 21:49:18 +0000

    New empty module loading

---
+++ src/Modules/Clustering/SpectralClusterModule.cs
@@ -0,0 +1,19 @@
+using Prism.Ioc;
+using Prism.Modularity;
+using System;
+
+namespace Clustering
+{
+    public class SpectralClusterModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+           
+        }
+    }
+}
